90446e980260bdfa2217736e9313e7525b2fef21
quyhn1l <quydeptrai21012002@gmail.com>
2024-01-06 21:14:34 +0700

    outroom bug

--- TCP_Server/auth/auth_handle.h
+++ TCP_Server/auth/auth_handle.h
@@ -2,11 +2,11 @@
 enum AuthStatus
 {
     LOGIN_SUCCESS,
-    LG_USER_BLOCK,
+    LG_OTHER_CLIENT,
     LG_USER_NOT_EXIST,
     INCORRECT_PASSWORD
 };
-enum AuthStatus login_handle();
+enum AuthStatus login_handle(char *username, char *password);
 int check_account_exist();
 // void logout_handle();
 int signup_handle();
